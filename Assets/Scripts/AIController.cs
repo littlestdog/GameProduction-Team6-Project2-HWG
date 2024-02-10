f34bb5455e66e711547ead99a83a69f6cb7470e6
Infinity <95947686+InfinitynBeyondd@users.noreply.github.com>
2024-02-10 13:00:11 -0500

    Adderd initial enemy AI and Prefab

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+    //Vector3 enemy; (Didn't work)
+
+    //States
+    public float sightRange;
+    public bool playerInSightRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (playerInSightRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patrolling();
+        }
+    }
+
+    //This code finds the Waypoints placed on the map
+    private void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+        Debug.Log("Updating the Location");
+    }
+    //This code resets the Waypoint Index for patrolling
+    private void IterateWaypointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+    //This code allows the enemy to patroll
+    private void Patrolling()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            IterateWaypointIndex();
+            UpdateDestination();
+            Debug.Log("Still Patrolling");
+        }
+    }
+
+    //This code allows the enemy to Chase
+    private void ChasePlayer()
+    {
+            agent.SetDestination(player.position);
+            Debug.Log("Chasing the Player"); 
+    }
+    //private void EnemyDestination() (Tried to band-aid it, but didn't work)
+    //{
+    //    enemy = player.position;
+    //    agent.SetDestination(enemy);
+    //}
+}
